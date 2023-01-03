5767d9a98627d5247d8f3662ae834da48b21c110
arcausin <arcausin@gmail.com>
2023-01-03 23:40:07 +0100

    make files example to config website

---
+++ config/database-example.php
@@ -0,0 +1,19 @@
+<?php
+function dbConnect() {
+    $host = '';
+    $dbname = '';
+    $charset = 'utf8mb4';
+    $username = '';
+    $password = '';
+
+    try {
+        $database = new PDO("mysql:host=$host;dbname=$dbname;charset=$charset", $username, $password);
+        // définit le mode d'erreur de PDO sur exception
+        $database->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    } catch(PDOException $e) {
+        // affiche un message d'erreur s'il y a eu un problème de connexion
+        echo 'Error: ' . $e->getMessage();
+    }
+
+    return $database;
+}
