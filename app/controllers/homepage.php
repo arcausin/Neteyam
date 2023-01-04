83de063281ac7962ca871826164595d6266c49a8
arcausin <arcausin@gmail.com>
2023-01-04 11:05:46 +0100

    add same dynamics for games like articles in homepage

--- app/controllers/homepage.php
+++ app/controllers/homepage.php
@@ -3,7 +3,7 @@
 require_once($_SERVER['DOCUMENT_ROOT'].'/app/models/games.php');
 require_once($_SERVER['DOCUMENT_ROOT'].'/app/models/articles.php');
 
-$games = getLastGames('0', 4);
+$lastGames = getLastGames('0', 4);
 $lastNews = getLastNews('0', 4);
 $lastReviews = getLastReviews('0', 4);
 $lastGuides = getLastGuides('0', 4);
--- app/views/homepage.php
+++ app/views/homepage.php
@@ -98,29 +98,27 @@
         <?php } ?>
     </div>
     <div class="col-12 col-lg-4 mt-3">
-        <div class="mb-3">
-            <a href="/jeux"><span class="badge text-bg-light py-2 fs-2 animate-opacity">Jeux</span></a>
-        </div>
-        
-        <div class="row g-3 mb-3">
-            <?php
-            foreach ($games as $game) {
-            ?>
-            <div class="col-6 col-md-3 col-lg-6">
-                <div class="shadow rounded">
-                    <a class="text-decoration-none text-white animate-opacity" href="/jeux/<?= $game['id_public']; ?>">
-                        <img class="img-fluid rounded-top" src="/public/img/games/<?= $game['illustration']; ?>" alt="">
-
-                        <div class="p-2">
-                            <h3 class="fs-6 text-center shortcut-word"><?= $game['title']; ?></h3>
-                        </div>
-                    </a>
-                </div>
+        <?php if ($lastGames) { ?>
+            <div class="mb-3">
+                <a href="/jeux"><span class="badge text-bg-light py-2 fs-2 animate-opacity">Jeux</span></a>
             </div>
-            <?php
-            }
-            ?>
-        </div>
+            
+            <div class="row g-3 mb-3">
+                <?php foreach ($lastGames as $lastGame) { ?>
+                <div class="col-6 col-md-3 col-lg-6">
+                    <div class="shadow rounded">
+                        <a class="text-decoration-none text-white animate-opacity" href="/jeux/<?= $lastGame['id_public']; ?>">
+                            <img class="img-fluid rounded-top" src="/public/img/games/<?= $lastGame['illustration']; ?>" alt="">
+
+                            <div class="p-2">
+                                <h3 class="fs-6 text-center shortcut-word"><?= $lastGame['title']; ?></h3>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <?php } ?>
+            </div>
+        <?php } ?>
     </div>
 </div>
 <?php $content = ob_get_clean(); ?>
