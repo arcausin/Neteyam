240da9eef742d98b03a2d562c5160fff7bffba25
arcausin <arcausin@gmail.com>
2023-01-22 14:00:14 +0100

    ajout de 2 jeux en plus sur la homepage

--- app/controllers/homepage.php
+++ app/controllers/homepage.php
@@ -6,7 +6,7 @@ $lastNews = getLastNews('0', 4);
 $lastReviews = getLastReviews('0', 4);
 $lastGuides = getLastGuides('0', 4);
 $lastFeatures = getLastFeatures('0', 4);
-$lastGames = getLastGames(3);
-$nextGames = getNextGames(3);
+$lastGames = getLastGames(4);
+$nextGames = getNextGames(4);
 
 require_once($_SERVER['DOCUMENT_ROOT'].'/app/views/homepage.php');
