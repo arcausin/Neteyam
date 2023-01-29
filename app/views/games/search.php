92be64346ffe05aa4d05bb487bfb61e48f905077
arcausin <arcausin@gmail.com>
2023-01-29 20:19:05 +0100

    correction design page recherche games

--- app/views/games/search.php
+++ app/views/games/search.php
@@ -1,8 +1,8 @@
-<div class="row">
+<div class="row g-4 mb-3">
     <?php
     foreach ($gamesSearch as $gameSearch) {
     ?>
-    <div class="col-6 col-md-3 col-lg-2 mb-3">
+    <div class="col-6 col-md-3 col-lg-2">
         <div class="shadow rounded">
             <a class="text-decoration-none text-white animate-opacity" href="/jeux/<?= $gameSearch['id_public']; ?>">
                 <img class="img-fluid rounded" src="/public/img/games/<?= $gameSearch['illustration']; ?>" alt="">
