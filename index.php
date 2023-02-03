11182ed144f41fc4c4a7562b83804e0bce52dc04
arcausin <arcausin@gmail.com>
2023-02-03 09:58:21 +0100

    sécurisation d'intrusion controller pour les non connectés

--- index.php
+++ index.php
@@ -21,7 +21,7 @@ if (strpos($host, 'www') === 0) {
 $urlNative = "https://" . $host;
 
 if ($url[0] == 'administration') {
-    if ($url[0] == 'administration' && empty($url[1])) {
+    if (!empty($_SESSION['user']) && $url[0] == 'administration' && empty($url[1])) {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/homepage.php');
     }
     elseif ($url[0] == 'administration' && $url[1] == 'inscription' && empty($url[2])) {
@@ -30,7 +30,7 @@ if ($url[0] == 'administration') {
     elseif ($url[0] == 'administration' && $url[1] == 'connexion' && empty($url[2])) {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/login.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'deconnexion' && empty($url[2])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'deconnexion' && empty($url[2])) {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/logout.php');
     }
     elseif ($url[0] == 'administration' && $url[1] == 'mot-de-passe-oublie' && empty($url[2])) {
@@ -41,123 +41,123 @@ if ($url[0] == 'administration') {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/new-password.php');
     }
 
-    elseif ($url[0] == 'administration' && $url[1] == 'articles' && empty($url[2])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'articles' && empty($url[2])) {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/articles/index.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'articles' && $url[2] == 'ajouter' && empty($url[3])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'articles' && $url[2] == 'ajouter' && empty($url[3])) {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/articles/create.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'articles' && !empty($url[2]) && empty($url[3])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'articles' && !empty($url[2]) && empty($url[3])) {
         $articleIdPublic = $url[2];
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/articles/read.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'articles' && $url[2] == 'modifier' && !empty($url[3]) && empty($url[4])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'articles' && $url[2] == 'modifier' && !empty($url[3]) && empty($url[4])) {
         $articleIdPublic = $url[3];
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/articles/update.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'articles' && $url[2] == 'supprimer' && !empty($url[3]) && empty($url[4])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'articles' && $url[2] == 'supprimer' && !empty($url[3]) && empty($url[4])) {
         $articleIdPublic = $url[3];
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/articles/delete.php');
     }
     
-    elseif ($url[0] == 'administration' && $url[1] == 'jeux' && empty($url[2])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'jeux' && empty($url[2])) {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/games/index.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'jeux' && $url[2] == 'ajouter' && empty($url[3])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'jeux' && $url[2] == 'ajouter' && empty($url[3])) {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/games/create.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'jeux' && !empty($url[2]) && empty($url[3])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'jeux' && !empty($url[2]) && empty($url[3])) {
         $gameIdPublic = $url[2];
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/games/read.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'jeux' && $url[2] == 'modifier' && !empty($url[3]) && empty($url[4])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'jeux' && $url[2] == 'modifier' && !empty($url[3]) && empty($url[4])) {
         $gameIdPublic = $url[3];
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/games/update.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'jeux' && $url[2] == 'supprimer' && !empty($url[3]) && empty($url[4])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'jeux' && $url[2] == 'supprimer' && !empty($url[3]) && empty($url[4])) {
         $gameIdPublic = $url[3];
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/games/delete.php');
     }
 
-    elseif ($url[0] == 'administration' && $url[1] == 'extensions' && empty($url[2])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'extensions' && empty($url[2])) {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/expansions/index.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'extensions' && $url[2] == 'ajouter' && empty($url[3])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'extensions' && $url[2] == 'ajouter' && empty($url[3])) {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/expansions/create.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'extensions' && !empty($url[2]) && empty($url[3])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'extensions' && !empty($url[2]) && empty($url[3])) {
         $expansionIdPublic = $url[2];
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/expansions/read.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'extensions' && $url[2] == 'modifier' && !empty($url[3]) && empty($url[4])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'extensions' && $url[2] == 'modifier' && !empty($url[3]) && empty($url[4])) {
         $expansionIdPublic = $url[3];
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/expansions/update.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'extensions' && $url[2] == 'supprimer' && !empty($url[3]) && empty($url[4])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'extensions' && $url[2] == 'supprimer' && !empty($url[3]) && empty($url[4])) {
         $expansionIdPublic = $url[3];
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/expansions/delete.php');
     }
 
-    elseif ($url[0] == 'administration' && $url[1] == 'consoles' && empty($url[2])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'consoles' && empty($url[2])) {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/consoles/index.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'consoles' && $url[2] == 'ajouter' && empty($url[3])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'consoles' && $url[2] == 'ajouter' && empty($url[3])) {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/consoles/create.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'consoles' && !empty($url[2]) && empty($url[3])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'consoles' && !empty($url[2]) && empty($url[3])) {
         $consoleIdPublic = $url[2];
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/consoles/read.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'consoles' && $url[2] == 'modifier' && !empty($url[3]) && empty($url[4])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'consoles' && $url[2] == 'modifier' && !empty($url[3]) && empty($url[4])) {
         $consoleIdPublic = $url[3];
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/consoles/update.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'consoles' && $url[2] == 'supprimer' && !empty($url[3]) && empty($url[4])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'consoles' && $url[2] == 'supprimer' && !empty($url[3]) && empty($url[4])) {
         $consoleIdPublic = $url[3];
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/consoles/delete.php');
     }
     
-    elseif ($url[0] == 'administration' && $url[1] == 'entreprises' && empty($url[2])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'entreprises' && empty($url[2])) {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/compagnies/index.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'entreprises' && $url[2] == 'ajouter' && empty($url[3])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'entreprises' && $url[2] == 'ajouter' && empty($url[3])) {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/compagnies/create.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'entreprises' && !empty($url[2]) && empty($url[3])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'entreprises' && !empty($url[2]) && empty($url[3])) {
         $companyIdPublic = $url[2];
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/compagnies/read.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'entreprises' && $url[2] == 'modifier' && !empty($url[3]) && empty($url[4])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'entreprises' && $url[2] == 'modifier' && !empty($url[3]) && empty($url[4])) {
         $companyIdPublic = $url[3];
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/compagnies/update.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'entreprises' && $url[2] == 'supprimer' && !empty($url[3]) && empty($url[4])) {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'entreprises' && $url[2] == 'supprimer' && !empty($url[3]) && empty($url[4])) {
         $companyIdPublic = $url[3];
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/compagnies/delete.php');
     }
     
-    elseif ($url[0] == 'administration' && $url[1] == 'documentation' && $url[2] == 'components' && $url[3] == 'buttons') {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'documentation' && $url[2] == 'components' && $url[3] == 'buttons') {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/documentation/components/buttons.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'documentation' && $url[2] == 'components' && $url[3] == 'cards') {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'documentation' && $url[2] == 'components' && $url[3] == 'cards') {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/documentation/components/cards.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'documentation' && $url[2] == 'utilities' && $url[3] == 'animations') {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'documentation' && $url[2] == 'utilities' && $url[3] == 'animations') {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/documentation/utilities/animations.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'documentation' && $url[2] == 'utilities' && $url[3] == 'borders') {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'documentation' && $url[2] == 'utilities' && $url[3] == 'borders') {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/documentation/utilities/borders.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'documentation' && $url[2] == 'utilities' && $url[3] == 'colors') {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'documentation' && $url[2] == 'utilities' && $url[3] == 'colors') {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/documentation/utilities/colors.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'documentation' && $url[2] == 'utilities' && $url[3] == 'others') {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'documentation' && $url[2] == 'utilities' && $url[3] == 'others') {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/documentation/utilities/others.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'documentation' && $url[2] == 'libraries' && $url[3] == 'chart') {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'documentation' && $url[2] == 'libraries' && $url[3] == 'chart') {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/documentation/libraries/chart.php');
     }
-    elseif ($url[0] == 'administration' && $url[1] == 'documentation' && $url[2] == 'libraries' && $url[3] == 'datatables') {
+    elseif (!empty($_SESSION['user']) && $url[0] == 'administration' && $url[1] == 'documentation' && $url[2] == 'libraries' && $url[3] == 'datatables') {
         require_once($_SERVER['DOCUMENT_ROOT'].'/admin/controllers/documentation/libraries/datatables.php');
     }
 
